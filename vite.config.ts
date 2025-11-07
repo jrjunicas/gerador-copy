31c089295d01073cf503b3e3c8872f25d3cd22e1
jrjunicas <72285032+jrjunicas@users.noreply.github.com>
2025-11-07 09:08:54 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/gerador-copy/", // 👈 ESSENCIAL para sites em subpasta!
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
-    },
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".css"]
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: false
-  }
+  base: '/gerador-copy/', // <- sua subpasta na Hostinger
 });
