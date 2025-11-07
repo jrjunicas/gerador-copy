bcfb26cca922113026b7cc86529b1f836fe94364
jrjunicas <72285032+jrjunicas@users.noreply.github.com>
2025-11-07 09:13:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gerador-copy/', // <- sua subpasta na Hostinger
+  base: '/gerador-copy/', // obrigatório se o site está em subpasta
 });
